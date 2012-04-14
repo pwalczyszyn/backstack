eb673ac5060149e423286b63e8aa0bded5f10fdb
Piotr Walczyszyn <piotr.walczyszyn@gmail.com>
2012-04-14 17:02:41 +0200

    - fixed lib initialization sequance

--- src/BackStack.js
+++ src/BackStack.js
@@ -6,21 +6,18 @@
  * Time: 2:11 PM
  */
 
+window.BackStack = {};
+
 define(['StackNavigator', 'StackView', 'effects/NoEffect', 'effects/SlideEffect', 'effects/FadeEffect'],
     function (StackNavigator, StackView, NoEffect, SlideEffect, FadeEffect) {
 
-        var BackStack = {
-            StackNavigator:StackNavigator,
-            StackView:StackView,
-            NoEffect:NoEffect,
-            SlideEffect:SlideEffect,
-            FadeEffect:FadeEffect
-        };
+        var BackStack = window.BackStack;
+        BackStack.StackNavigator = StackNavigator;
+        BackStack.StackView = StackView;
+        BackStack.NoEffect = NoEffect;
+        BackStack.SlideEffect = SlideEffect;
+        BackStack.FadeEffect = FadeEffect;
 
         return BackStack;
     });
 
-require(['BackStack'], function (BackStack) {
-    window.BackStack = BackStack;
-    $(document).trigger('BackStackLoaded');
-});
