15bf30c20fdafb2c70abf1b8af2404347bad133e
Piotr Walczyszyn <piotr.walczyszyn@gmail.com>
2012-04-16 19:33:57 +0200

    - added extendable Effect class

--- src/BackStack.js
+++ src/BackStack.js
@@ -1,8 +1,9 @@
-define(['StackNavigator', 'StackView', 'effects/NoEffect', 'effects/SlideEffect', 'effects/FadeEffect'],
-    function (StackNavigator, StackView, NoEffect, SlideEffect, FadeEffect) {
+define(['StackNavigator', 'StackView', 'effects/Effect', 'effects/NoEffect', 'effects/SlideEffect', 'effects/FadeEffect'],
+    function (StackNavigator, StackView, Effect, NoEffect, SlideEffect, FadeEffect) {
 
         BackStack.StackNavigator = StackNavigator;
         BackStack.StackView = StackView;
+        BackStack.Effect = Effect;
         BackStack.NoEffect = NoEffect;
         BackStack.SlideEffect = SlideEffect;
         BackStack.FadeEffect = FadeEffect;
